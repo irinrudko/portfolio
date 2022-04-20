c4eac076d1aafcb6e4f76ba3b42b481d14be100b
Iryna Rudzko <irinrudko@gmail.com>
2022-04-20 13:56:28 +0300

    refactor: refactor styles in Project component

--- src/Projects/Project/Project.js
+++ src/Projects/Project/Project.js
@@ -5,14 +5,11 @@ import style from './Project.module.css'
 export const Project = (props) => {
     return <article className={style.project}>
                <div className={style.imgContainer}>
-                    <img href="#!" alt="ico"></img>
+                    <a href="#!"><h3 className={style.link}>Check it out</h3></a>
                 </div>
                 <div className={style.contentContainer}>
-                    <a href="#!"><h3 className={style.link}>Check it out</h3></a>
-                    <div className={style.description}>
                         <h4>{props.title}</h4>
                         <span>{props.description}</span>
-                    </div>
                 </div>             
             </article>   
 }
