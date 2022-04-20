6519889bf1fb50500404d5c003ae78534afed3f4
Iryna Rudzko <irinrudko@gmail.com>
2022-04-20 21:51:11 +0300

    refactor: remove not necessary div

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -5,15 +5,13 @@ import commonStyle from '../common/styles/Styles.module.css'
 export const Contacts = () => {
     return <section className={style.item}>
                 <div className={`${commonStyle.container} ${style.contactsContainer}`}>
-                    <h2 className={commonStyle.title}>Contacts</h2>
-                    <article className={style.contacts}>
-                        <form action="#!" className={style.form}>
-                            <input placeholder="Your first name" /> 
-                            <input  placeholder="Your last name" />
-                            <textarea placeholder="Write any additional info here"></textarea>
-                        </form>
-                        <input type="submit" value="Submit" className={style.submit} />
-                    </article>
+                    <h2 className={commonStyle.title}>Contact me</h2>
+                    <form action="#!" className={style.form}>
+                        <input placeholder="Your first name" /> 
+                        <input  placeholder="Your last name" />
+                        <textarea placeholder="Your message here"></textarea>
+                        <input type="submit" value="Submit" className={style.submit}/>
+                    </form>
                 </div>
             </section>
 }
