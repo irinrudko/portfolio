6f920dace54b4ae91c913950ae5b9619efc9f8ec
Iryna Rudzko <irinrudko@gmail.com>
2022-04-27 22:16:56 +0300

    feat: add styles to Title

--- src/MySkills/MySkills.jsx
+++ src/MySkills/MySkills.jsx
@@ -1,22 +1,22 @@
-import React from "react"
-import style from './MySkills.module.css'
+import React from 'react'
+import style from './MySkills.module.scss'
 import commonStyle from '../common/styles/Styles.module.css'
-import { Skill } from "./Skill/Skill"
-
+import { Skill } from './Skill/Skill'
+import { Title } from '../common/components/Title/Title'
 
 export const MySkills = () => {
     return <div className={style.item}>
-                <section className={`${commonStyle.container} ${style.skillsContainer}`}>
-                        <h2 className={commonStyle.title}>My Skills</h2>
-                        <div className={style.skills}>
-                            <Skill title={'React'} description={'about it'} />
-                            <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '}/>
-                            <Skill title={'CSS'} description={'about it'}/>
-                            <Skill title={'Storybook'} description={'about it'}/>
-                            <Skill title={'Redux'} description={'about it'}/>
-                            <Skill title={'Etc.'} description={'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
-                        </div>
-                 </section>
+        <section className={`${commonStyle.container} ${style.skillsContainer}`}>
+            <Title title={'My '} subtitle={'skills'} bgTitle={'About me'} />
+            <div className={style.skills}>
+                <Skill title={'React'} description={'about it'} />
+                <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '} />
+                <Skill title={'CSS'} description={'about it'} />
+                <Skill title={'Storybook'} description={'about it'} />
+                <Skill title={'Redux'} description={'about it'} />
+                <Skill title={'Etc.'} description={'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'} />
             </div>
+        </section>
+    </div>
 }
 
--- src/common/components/Title/Title.jsx
+++ src/common/components/Title/Title.jsx
@@ -3,5 +3,8 @@ import style from './Title.module.scss'
 
 
 export const Title = (props) => {
-    return <h2 className={style.title}>{props.title}<span className={style.subtitle}>{props.subtitle}</span></h2>
+    return <h2 className={style.title}>{props.title}
+        <span className={style.subtitle}>{props.subtitle}</span>
+        <span className={style.bgTitle}>{props.bgTitle}</span>
+    </h2>
 }
