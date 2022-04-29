47b8f502b90ab1ec25a5a985ecbd450f67f86802
Iryna Rudzko <irinrudko@gmail.com>
2022-04-29 22:32:23 +0300

    feat: add basic styling to Contacts component

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -1,18 +1,21 @@
 import React from "react"
-import style from './Contacts.module.css'
+import style from './Contacts.module.scss'
 import commonStyle from '../common/styles/Styles.module.css'
 import { Title } from '../common/components/Title/Title'
+import { Footer } from '../Footer/Footer';
+
 
 export const Contacts = () => {
     return <section className={style.item}>
         <div className={`${commonStyle.container} ${style.contactsContainer}`}>
             <Title title={'contact '} subtitle={'me'} bgTitle={'feel free to'} />
             <form action="#!" className={style.form}>
-                <input placeholder="Your first name" />
-                <input placeholder="Your last name" />
-                <textarea placeholder="Your message here"></textarea>
+                <input placeholder="Your name" required />
+                <input type='email' placeholder="Your email name" required />
+                <textarea placeholder="Your message here" required></textarea>
                 <input type="submit" value="Submit" className={style.submit} />
             </form>
         </div>
+        {/* <Footer /> */}
     </section>
 }
