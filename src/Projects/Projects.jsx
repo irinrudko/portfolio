7078717b4d2e84c265fbd066042b2a5d3d108f62
Iryna Rudzko <irinrudko@gmail.com>
2022-10-15 21:27:03 +0400

    feat: add links to projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -8,35 +8,58 @@ import todolistImg from '../assets/images/todolist.png'
 import virtualPianoImg from '../assets/images/virtualPiano.png'
 import petStoryImg from '../assets/images/petstory.png'
 
-
 export const Projects = () => {
     const socialNetwork = {
         backgroundImage: `url(${myNetImg})`,
-    };
+    }
     const todolist = {
         backgroundImage: `url(${todolistImg})`,
-    };
+    }
     const virtualPiano = {
         backgroundImage: `url(${virtualPianoImg})`,
-    };
+    }
     const petStory = {
         backgroundImage: `url(${petStoryImg})`,
-    };
+    }
 
-    return <div className={style.item}>
-        <section className={`${commonStyle.container} ${style.projectsContainer}`}>
-            <Title title={'my '} subtitle={'projects'} bgTitle={'portfolio'} />
-            <div className={style.projects}>
-                <a href='#!'>
-                    <Project title='Social Network' description='Model-View-Presenter, Flux patterns; React Redux, Class &amp; Func components, TS' style={socialNetwork} /></a>
-                <a href='#!' target='_blank'>
-                    <Project title='Todolist' description='SPA – React, Redux, Typescript, Storybook, hooks, HOCs, REST API, axios. Unit, integration tests' style={todolist} /></a>
-                <a href='https://irinrudko.github.io/virtual-piano/' target='_blank'>
-                    <Project title='Virtual piano keyboard' description='Vanilla JS. DOM events' style={virtualPiano} /></a>
-                <a href='https://rolling-scopes-school.github.io/irinrudko-JSFE2021Q1/online-zoo/pages/index.html' target='_blank'>
-                    <Project title='Petstory figma mockup' description='SASS, svg, adaptive, animation, tooltip, pseudo, position sticky, absolute, bg images' style={petStory} /> </a>
-            </div>
-        </section>
-    </div>
+    return (
+        <div className={style.item}>
+            <section className={`${commonStyle.container} ${style.projectsContainer}`}>
+                <Title title={'my '} subtitle={'projects'} bgTitle={'portfolio'} />
+                <div className={style.projects}>
+                    <a href="https://my-net.vercel.app/">
+                        <Project
+                            title="Social Network"
+                            description="Model-View-Presenter, Flux patterns; React Redux, Class &amp; Func components, TS"
+                            style={socialNetwork}
+                        />
+                    </a>
+                    <a href="https://todolist-irinrudko.vercel.app/" target="_blank">
+                        <Project
+                            title="Todolist"
+                            description="SPA – React, Redux, Typescript, Storybook, hooks, HOCs, REST API, axios. Unit, integration tests"
+                            style={todolist}
+                        />
+                    </a>
+                    <a href="https://irinrudko.github.io/virtual-piano/" target="_blank">
+                        <Project
+                            title="Virtual piano keyboard"
+                            description="Vanilla JS. DOM events"
+                            style={virtualPiano}
+                        />
+                    </a>
+                    <a
+                        href="https://rolling-scopes-school.github.io/irinrudko-JSFE2021Q1/online-zoo/pages/index.html"
+                        target="_blank"
+                    >
+                        <Project
+                            title="Petstory figma mockup"
+                            description="SASS, svg, adaptive, animation, tooltip, pseudo, position sticky, absolute, bg images"
+                            style={petStory}
+                        />{' '}
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
 }
-
