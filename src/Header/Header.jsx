ddae91da665180be3036138b2ff56e525f098a0a
Iryna Rudzko <irinrudko@gmail.com>
2022-10-15 21:18:42 +0400

    refactor: change homepage in package.json

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 
 export const Header = () => {
     return (<ul className={style.header}>
-        <NavLink to='/'><li className={style.navItem}><FontAwesomeIcon icon={faHome} className={style.ico} /><h2 className={style.title}>Home</h2></li></NavLink>
+        <NavLink to='/' ><li className={style.navItem}><FontAwesomeIcon icon={faHome} className={style.ico} /><h2 className={style.title}>Home</h2></li></NavLink>
         <NavLink to='/skills'><li className={style.navItem}><FontAwesomeIcon icon={faBookOpen} className={style.ico} /><h2 className={style.title}>About me</h2></li></NavLink>
         <NavLink to='/projects'><li className={style.navItem}><FontAwesomeIcon icon={faBriefcase} className={style.ico} /><h2 className={style.title}>Projects</h2></li></NavLink>
         <NavLink to='/contacts'><li className={style.navItem}><FontAwesomeIcon icon={faPaperPlane} className={style.ico} /><h2 className={style.title}>Contacts</h2></li></NavLink>
