dc436daee2a6528d6924da3972f047d45d9cedbd
Iryna Rudzko <irinrudko@gmail.com>
2022-10-22 01:29:45 +0400

    fix: fix input text

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -1,25 +1,26 @@
-import React from "react"
+import React from 'react'
 import style from './Contacts.module.scss'
 import commonStyle from '../common/styles/Styles.module.css'
 import { Title } from '../common/components/Title/Title'
-import { Footer } from './Footer/Footer';
-import { Button } from "../common/components/Button/Button";
+import { Footer } from './Footer/Footer'
+import { Button } from '../common/components/Button/Button'
 import { faPaperPlane } from '@fortawesome/fontawesome-free-solid'
 
-
 export const Contacts = () => {
-    return <section className={style.item}>
-        <div className={`${commonStyle.container} ${style.contactsContainer}`}>
-            <Title title={'contact '} subtitle={'me'} bgTitle={'feel free to'} />
-            <div className={style.contentContainer}>
-                <Footer />
-                <form action="#!" className={style.form}>
-                    <input placeholder="Your name" required />
-                    <input type='email' placeholder="Your email name" required />
-                    <textarea placeholder="Your message here" required></textarea>
-                    <Button text='Send message' ico={faPaperPlane} type="submit" className={style.submit} />
-                </form>
+    return (
+        <section className={style.item}>
+            <div className={`${commonStyle.container} ${style.contactsContainer}`}>
+                <Title title={'contact '} subtitle={'me'} bgTitle={'feel free to'} />
+                <div className={style.contentContainer}>
+                    <Footer />
+                    <form action="#!" className={style.form}>
+                        <input placeholder="Your name" required />
+                        <input type="email" placeholder="Your email" required />
+                        <textarea placeholder="Your message here" required></textarea>
+                        <Button text="Send message" ico={faPaperPlane} type="submit" className={style.submit} />
+                    </form>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    )
 }
