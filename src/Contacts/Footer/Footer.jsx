5cc90d8dfa33d98a952ad787a52ad7e122531bc6
Iryna Rudzko <irinrudko@gmail.com>
2022-10-22 01:23:53 +0400

    feat: add contacts

--- src/Contacts/Footer/Footer.jsx
+++ src/Contacts/Footer/Footer.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React from 'react'
 import style from './Footer.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTelegram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/fontawesome-free-solid'
 
-
 export const Footer = () => {
-    return <footer className={style.item}>
-        <a href='!#' target='_blank' rel='author'>
-            <span><FontAwesomeIcon icon={faTelegram} />Telegram </span>
-        </a>
-        <a href='src/Contacts/Footer/Footer' target='_blank' rel='author'>
-            <span><FontAwesomeIcon icon={faLinkedin} />Linkedin </span>
-        </a>
-        <a href='src/Contacts/Footer/Footer' target='_blank' rel='author'>
-            <span><FontAwesomeIcon icon={faGithub} />GitHub </span>
-        </a>
-        <a href='!#' target='_blank' rel='author'>
-            <span><FontAwesomeIcon icon={faEnvelope} />Email </span>
-        </a>
-
-    </footer>
+    return (
+        <footer className={style.item}>
+            <a href="https://t.me/irinrudko" target="_blank" rel="author">
+                <span>
+                    <FontAwesomeIcon icon={faTelegram} />
+                    Telegram{' '}
+                </span>
+            </a>
+            <a href="https://www.linkedin.com/in/irinrudko/" target="_blank" rel="author">
+                <span>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    Linkedin{' '}
+                </span>
+            </a>
+            <a href="https://github.com/irinrudko/" target="_blank" rel="author">
+                <span>
+                    <FontAwesomeIcon icon={faGithub} />
+                    GitHub{' '}
+                </span>
+            </a>
+            <a href="mailto:irinrudko@gmail.com" rel="author">
+                <span>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    Email{' '}
+                </span>
+            </a>
+        </footer>
+    )
 }
