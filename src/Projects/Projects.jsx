8ddb996cddc011e7052e9baab7e5b965fc496ab7
Iryna Rudzko <irinrudko@gmail.com>
2022-10-22 12:39:28 +0400

    fix: update link to social net project

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -38,7 +38,7 @@ export const Projects = () => {
                             style={cards}
                         />
                     </a>
-                    <a href="https://my-net-irinrudko.vercel.app/" target="_blank">
+                    <a href="https://irinrudko.github.io/social-network/" target="_blank">
                         <Project
                             title="Social Network"
                             description="Model-View-Presenter, Flux patterns; React Redux, Class &amp; Func components, TS"
